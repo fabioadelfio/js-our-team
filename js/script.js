42d443f7dfa7a46e6265e8ebbfdd953783a2048d
fabioadelfio <adelfiofabio07@gmail.com>
2025-04-15 14:25:37 +0200

    generateCards

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const teamContainer = document.getElementById(`team-container`);
+
 const teamMembers = [
   {
     name: "Marco Bianchi",
@@ -36,3 +38,30 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+const generateMemberCardHTML = (name, role, email, img) => {
+  return `<div class="col-4">
+            <div class="card mb-3" >
+              <div class="row g-0 bg-black text-light">
+                <div class="col-4">
+                  <img src="${img}" class="img-fluid rounded-start" alt="${name}">
+                </div>
+               <div class="col-8">
+                  <div class="card-body">
+                    <h5 id="fullname" class="card-title">${name}</h5>
+                    <p id="role" class="card-text">${role}</p>
+                    <p id="email" class="card-text"><small><a href="#">${email}</a></small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`
+}
+
+let cardsHTML = '';
+
+for(const member of teamMembers) {
+  const { name, role, email, img } = member;
+  cardsHTML += generateMemberCardHTML(name, role, email, img);
+  teamContainer.innerHTML = cardsHTML;
+}
